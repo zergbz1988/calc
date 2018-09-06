87fe4f9151f34011743593b2b31b6513669287c5
komarov <zergbz1988@gmail.com>
2018-09-06 19:55:07 +0300

    Default Calculator

--- src/DefaultCalc.php
+++ src/DefaultCalc.php
@@ -17,6 +17,7 @@ use Zergbz1988\Calc\Contracts\Calc;
 class DefaultCalc implements Calc
 {
     private $calcResult;
+
     /**
      * DefaultCalc constructor.
      */
@@ -31,44 +32,175 @@ class DefaultCalc implements Calc
      */
     public function getCalcResult(string $input): CalcResult
     {
-        $calcArray = preg_split('/([+-\/()]{1})/', $input, -1, PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE);
+        $input = str_replace(' ', '', $input);
+        $inputStack = preg_split('/([+-\/*()\^]{1})/', $input, -1, PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE);
 
-        foreach ($calcArray as $word) {
-            if ($this->isOperator($word)) {
+        if (empty($inputStack)) {
+            return $this->errorResult();
+        }
 
+        $out = $this->getExpression($inputStack);
+
+        if (empty($out)) {
+            return $this->errorResult();
+        }
+
+        $result = $this->counting($out);
+
+        if (empty($result)) {
+            return $this->errorResult();
+        }
+
+        $result = (string) reset($result);
+
+        return $this->okResult($result);
+    }
+
+    /**
+     * @param array $input
+     * @return array
+     */
+    private function getExpression(array $input) : array
+    {
+        $out = [];
+        $stack = [];
+
+        while ($word = array_shift($input)) {
+            if (is_numeric($word)) {
+                array_push($out, $word);
             } elseif ($this->isRoundBrackets($word)) {
-
-            } elseif (is_numeric($word)) {
-
+                if ($word === '(') {
+                    array_push($stack, $word);
+                } else {
+                    while (($tmp = array_pop($stack)) !== '(') {
+                        array_push($out, $tmp);
+                    }
+                }
+            } elseif ($this->isOperator($word)) {
+                for ($j = count($stack) - 1; $j >= 0; --$j) {
+                    if ($this->getPriority($stack[$j]) < $this->getPriority($word)) {
+                        break;
+                    }
+                    $out[] = $stack[$j];
+                    unset($stack[$j]);
+                }
+                $stack = array_values($stack);
+                array_push($stack, $word);
             } else {
-                $this->calcResult->setErrorStatus();
-                $this->calcResult->setMessage('Передан неправильный аргумент для вычисления!');
-
-                return $this->calcResult;
+                return [];
             };
         }
 
-        $this->calcResult->setOkStatus();
-        $this->calcResult->setMessage('Результат');
+        if ($stack) {
+            $out = array_merge($out, array_reverse($stack));
+        }
 
+        return $out;
+    }
+
+    /**
+     * @param array $input
+     * @return array
+     */
+    private function counting(array $input) : array
+    {
+        $out = [];
+
+        while ($word = array_shift($input)) {
+            if (is_numeric($word)) {
+                array_push($out, $word);
+            } else {
+                $num1 = array_pop($out);
+                $num2 = array_pop($out);
+                switch ($word) {
+                    case '+':
+                        $tmp = $num2 + $num1;
+                        break;
+                    case '-':
+                        $tmp = $num2 - $num1;
+                        break;
+                    case '*':
+                        $tmp = $num2 * $num1;
+                        break;
+                    case '/':
+                        $tmp = $num2 / $num1;
+                        break;
+                    case '^':
+                        $tmp = pow($num2, $num1);
+                        break;
+                    default:
+                        $tmp = $num2 + $num1;
+                }
+                array_push($out, $tmp);
+            }
+        }
+
+        return $out;
+    }
+
+    /**
+     * @param string $word
+     * @return bool
+     */
+    private function isOperator(string $word): bool
+    {
+        return in_array($word, ['+', '-', '*', '/', '^']);
+    }
+
+    /**
+     * @param string $word
+     * @return bool
+     */
+    private function isRoundBrackets(string $word): bool
+    {
+        return in_array($word, ['(', ')']);
+    }
+
+    /**
+     * @param string $operator
+     * @return int
+     */
+    private function getPriority(string $operator) : int
+    {
+        switch ($operator) {
+            case '(':
+                return 0;
+            case ')':
+                return 1;
+            case '+':
+                return 2;
+            case '-':
+                return 3;
+            case '*':
+                return 4;
+            case '/':
+                return 4;
+            case '^':
+                return 5;
+            default:
+                return 6;
+        }
+    }
+
+    /**
+     * @param string $message
+     * @return CalcResult
+     */
+    private function errorResult(string $message = 'Передан неправильный аргумент для вычисления!'): CalcResult
+    {
+        $this->calcResult->setErrorStatus();
+        $this->calcResult->setMessage($message);
         return $this->calcResult;
     }
 
     /**
-     * @param string $word
-     * @return bool
+     * @param string $result
+     * @return CalcResult
      */
-    private function isOperator(string $word) : bool
+    private function okResult(string $result): CalcResult
     {
-        return in_array($word, ['+', '-', '*', '/']);
-    }
-
-    /**
-     * @param string $word
-     * @return bool
-     */
-    private function isRoundBrackets(string $word) : bool
-    {
-        return in_array($word, ['(', ')']);
+        $this->calcResult->setOkStatus();
+        $this->calcResult->setMessage($result);
+        return $this->calcResult;
     }
 }
