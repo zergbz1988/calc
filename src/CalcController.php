63e3ab1aeb3cb9094e11e23415feb4a71ea59f48
zergbz1988 <zergbz1988@gmail.com>
2018-09-06 23:21:33 +0300

    add controller and simple validation

---
+++ src/CalcController.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Marat
+ * Date: 06.09.2018
+ * Time: 23:15
+ */
+
+namespace Zergbz1988\Calc;
+
+use Illuminate\Routing\Controller;
+use Zergbz1988\Calc\Interfaces\Calc;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class CalcController extends Controller
+{
+    /**
+     * @param Request $request
+     * @param Calc $calc
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function calc(Request $request, Calc $calc)
+    {
+        $validator = Validator::make($request->all(), ['input' => 'require|string']);
+
+        if ($validator->fails()) {
+            $calcResult = new CalcResult();
+            $calcResult->setErrorStatus();
+            $calcResult->setMessage($validator->errors()->first());
+
+            return response()->json([
+                'status' => $calcResult->getStatus(),
+                'message' => $calcResult->getMessage()
+            ]);
+        }
+
+        $input = $request->input('input');
+        $calcResult = $calc->getCalcResult($input);
+
+        return response()->json([
+            'status' => $calcResult->getStatus(),
+            'message' => $calcResult->getMessage()
+        ]);
+    }
+}
--- src/routes.php
+++ src/routes.php
@@ -1,16 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use Zergbz1988\Calc\Interfaces\Calc;
 
-Route::get('calc', function (Request $request, Calc $calc) {
-    $input = $request->input('input');
 
-    $calcResult = $calc->getCalcResult($input);
-
-    return response()->json([
-        'status' => $calcResult->getStatus(),
-        'message' => $calcResult->getMessage()
-    ]);
-});
+Route::get('calc', 'CalcController@calc');
