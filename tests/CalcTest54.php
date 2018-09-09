bfbd8021cf17bcfac15c01438d0c6fc1098e6ce8
zergbz1988 <zergbz1988@gmail.com>
2018-09-10 00:06:15 +0300

    removed console tests from 5.3-5.4

---
+++ tests/CalcTest54.php
@@ -0,0 +1,257 @@
+<?php
+
+use Orchestra\Testbench\TestCase;
+use Zergbz1988\Calc\Interfaces\Calc;
+
+/**
+ * Class CalcTest
+ * @property Calc $calc
+ *
+ */
+class CalcTest54 extends TestCase
+{
+    protected $calc = null;
+
+    public function getPackageProviders($app)
+    {
+        return ['Zergbz1988\Calc\CalcServiceProvider'];
+    }
+
+    /**
+     *  зададим значение Zergbz1988\Calc\Interfaces\Calc $calc
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $this->calc = resolve('Zergbz1988\Calc\Interfaces\Calc');
+    }
+
+    /**** UNIT TESTS ****/
+
+    /**
+     * Проверка пустого значения
+     */
+    public function testEmptyInput()
+    {
+        $this->assertEquals(
+            [
+                'status' => 'error',
+                'message' => 'Необходимо указать строковое значение поля input'
+            ],
+            $this->calc->getCalcResult('')->toArray()
+        );
+    }
+
+    /**
+     * Проверка неправильного значения
+     */
+    public function testInvalidInput()
+    {
+        $this->assertEquals(
+            [
+                'status' => 'error',
+                'message' => 'Передан неправильный аргумент для вычисления!'
+            ],
+            $this->calc->getCalcResult('7*20+3)')->toArray()
+        );
+    }
+
+    /**
+     * Проверка сложения
+     */
+    public function testAdd()
+    {
+        $this->assertEquals(
+            [
+                'status' => 'ok',
+                'message' => '5'
+            ],
+            $this->calc->getCalcResult('3+2')->toArray()
+        );
+    }
+
+    /**
+     * Проверка вычитания
+     */
+    public function testSub()
+    {
+        $this->assertEquals(
+            [
+                'status' => 'ok',
+                'message' => '1'
+            ],
+            $this->calc->getCalcResult('3-2')->toArray()
+        );
+    }
+
+    /**
+     * Проверка умножения
+     */
+    public function testMulti()
+    {
+        $this->assertEquals(
+            [
+                'status' => 'ok',
+                'message' => '6'
+            ],
+            $this->calc->getCalcResult('3*2')->toArray()
+        );
+    }
+
+    /**
+     * Проверка деления
+     */
+    public function testDiv()
+    {
+        $this->assertEquals(
+            [
+                'status' => 'ok',
+                'message' => '1.5'
+            ],
+            $this->calc->getCalcResult('3/2')->toArray()
+        );
+    }
+
+    /**
+     * Проверка возведения в степень
+     */
+    public function testPow()
+    {
+        $this->assertEquals(
+            [
+                'status' => 'ok',
+                'message' => '9'
+            ],
+            $this->calc->getCalcResult('3^2')->toArray()
+        );
+    }
+
+    /**
+     * Проверка приоритетности выполнения операций
+     */
+    public function testPriority()
+    {
+        $this->assertEquals(
+            [
+                'status' => 'ok',
+                'message' => '35'
+            ],
+            $this->calc->getCalcResult('3+2*4^2')->toArray()
+        );
+    }
+
+    /**
+     * Проверка круглых скобок
+     */
+    public function testBrackets()
+    {
+        $this->assertEquals(
+            [
+                'status' => 'ok',
+                'message' => '80'
+            ],
+            $this->calc->getCalcResult('(3+2)*4^2')->toArray()
+        );
+    }
+
+    /**** FUNCTIONAL TESTS ****/
+
+    /**
+     * Проверка пустого значения в браузере
+     */
+    public function testEmptyInputInBrowser()
+    {
+        $this->get('/calc?input=')->assertExactJson([
+            'status' => 'error',
+            'message' => 'Необходимо указать строковое значение поля input'
+        ]);
+    }
+
+    /**
+     * Проверка неправильного значения в браузере
+     */
+    public function testInvalidInputInBrowser()
+    {
+        $this->get('/calc?input=7*20+3)')->assertExactJson([
+            'status' => 'error',
+            'message' => 'Передан неправильный аргумент для вычисления!'
+        ]);
+    }
+
+    /**
+     * Проверка сложения в браузере
+     */
+    public function testAddInBrowser()
+    {
+        $this->get('/calc?input=3+2')->assertExactJson([
+            'status' => 'ok',
+            'message' => '5'
+        ]);
+    }
+
+    /**
+     * Проверка вычитания в браузере
+     */
+    public function testSubInBrowser()
+    {
+        $this->get('/calc?input=3-2')->assertExactJson([
+            'status' => 'ok',
+            'message' => '1'
+        ]);
+    }
+
+    /**
+     * Проверка умножения в браузере
+     */
+    public function testMultiInBrowser()
+    {
+        $this->get('/calc?input=3*2')->assertExactJson([
+            'status' => 'ok',
+            'message' => '6'
+        ]);
+    }
+
+    /**
+     * Проверка деления в браузере
+     */
+    public function testDivInBrowser()
+    {
+        $this->get('/calc?input=3/2')->assertExactJson([
+            'status' => 'ok',
+            'message' => '1.5'
+        ]);
+    }
+
+    /**
+     * Проверка возведения в степень в браузере
+     */
+    public function testPowInBrowser()
+    {
+        $this->get('/calc?input=3^2')->assertExactJson([
+            'status' => 'ok',
+            'message' => '9'
+        ]);
+    }
+
+    /**
+     * Проверка приоритетности выполнения операций в браузере
+     */
+    public function testPriorityInBrowser()
+    {
+        $this->get('/calc?input=3+2*4^2')->assertExactJson([
+            'status' => 'ok',
+            'message' => '35'
+        ]);
+    }
+
+    /**
+     * Проверка круглых скобок в браузере
+     */
+    public function testBracketsInBrowser()
+    {
+        $this->get('/calc?input=(3+2)*4^2')->assertExactJson([
+            'status' => 'ok',
+            'message' => '80'
+        ]);
+    }
+}
