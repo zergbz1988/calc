80f76fd771c67dbcda2ae99d7bb505b5ea1bf9bd
zergbz1988 <zergbz1988@gmail.com>
2018-09-09 23:07:34 +0300

    5.4 #4

--- src/CalcServiceProvider.php
+++ src/CalcServiceProvider.php
@@ -27,7 +27,7 @@ class CalcServiceProvider extends ServiceProvider
             __DIR__ . '/../config/calc.php', 'calc'
         );
 
-        $this->publishes([__DIR__ . '/../config/calc.php' => config_path('calc.php')]);
+        $this->publishes([__DIR__ . '/../config/calc.php' => app()->make('path.config').(DIRECTORY_SEPARATOR . 'calc.php')]);
 
         $calcClass = config('calc.calcClass');
 
